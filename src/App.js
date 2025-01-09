45ac0c1770040e5dd32d66f8bb84e43dd3f0f19a
Sharath Heralagi <106471249+Sharatheralagi007@users.noreply.github.com>
2025-01-09 12:44:59 +0530

    Quiz commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import Quizapp from './Quizapp'
+import React from "react";
+import Quizapp from "./Quizapp";
 
 export default function App() {
   return (
     <div>
+      <b
+        style={{
+          color: "wheat",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <h1>QUIZ ON REDUX</h1>
+      </b>
+
       <Quizapp />
     </div>
-  )
+  );
 }
